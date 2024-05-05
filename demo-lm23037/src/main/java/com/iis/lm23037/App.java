965c62120efa14510b6742c7ceb0128c472a293a
luislopez14 <lm23037@ues.edu.sv>
2024-05-05 17:02:14 -0600

    My first commit

---
+++ demo-lm23037/src/main/java/com/iis/lm23037/App.java
@@ -0,0 +1,17 @@
+package com.iis.lm23037;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello () {
+		return "Hello World";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello() );
+    }
+}
